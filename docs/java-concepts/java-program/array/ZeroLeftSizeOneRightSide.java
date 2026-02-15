e82b47b46874a7d96ec593bc738c18f87c3ee68e
sonawaneu777 <105368090+sonawaneu777@users.noreply.github.com>
2026-02-15 22:50:15 +0530

    Update ZeroLeftSizeOneRightSide.java

--- docs/java-concepts/java-program/array/ZeroLeftSizeOneRightSide.java
+++ docs/java-concepts/java-program/array/ZeroLeftSizeOneRightSide.java
@@ -1,27 +1,34 @@
-package org.array;
-public class ZeroLeftSizeOneRightSide {
+package listeners;
+import java.util.ArrayList;
+
+public class TestClass4 {
+	
 	public static void main(String[] args) {
-		int [] ar= {1,0,0,1,1,1,0,0,0};
+		int[] ar = { 1, 0, 0, 1, 1, 1, 0, 0, 0 };
 		int[] ar1 = separate0s1sSolution1(ar);
-		for(int i=0;i<ar1.length;i++) {
+		for (int i = 0; i < ar1.length; i++) {
 			System.out.print(ar1[i]);
 		}
 	}
-	public static int[] separate0s1sSolution1(int arr[])
-	 {
-	  int count=0;
-	  for (int i = 0; i < arr.length; i++) {
-	   if(arr[i]==0)
-	   {
-	    count++;
-	   }
-	  }
-	  for (int i = 0; i < count; i++) {
-	   arr[i]=0;
-	  }
-	  for (int i = count; i < arr.length; i++) {
-	   arr[i]=1;
-	  }
-	  return arr;
-	 }
+
+	public static int[] separate0s1sSolution1(int arr[]) {
+		int count = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == 0) {
+				count++;
+			}
+		}
+		for (int i = 0; i < count; i++) {
+			arr[i] = 0;
+		}
+		for (int i = count; i < arr.length; i++) {
+			arr[i] = 1;
+		}
+		return arr;
+	}
 }
+
+/*
+Output:
+000001111
+*/
